fb5006735bcb70923c2c57b62dd8e2d00ea0d065
Sergey Bezrukov <sb700sb@yandex.ru>
2023-07-29 15:51:17 +0300

    Исправить баги

--- controllers/users.js
+++ controllers/users.js
@@ -15,75 +15,75 @@ const getUsers = (req, res) => {
 const getUser = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
-  .then((users) => {
-    if (!users) {
-      res.status(NOT_FOUND_ERROR_STATUS).send({ message: 'Пользователь не найден' });
-      return;
-    }
-    res.send({ data: users });
-  })
-  .catch((err) => {
-    if (err.name === 'CastError') {
-      res.status(BAD_REQUEST_ERROR_STATUS).send({ message: 'Пользователь с указанным ID не найден' });
-    } else {
-      res.status(INTERNAL_SERVER_ERROR_STATUS).send({ message: 'Произошла ошибка' });
-    }
-  });
+    .then((users) => {
+      if (!users) {
+        res.status(NOT_FOUND_ERROR_STATUS).send({ message: 'Пользователь не найден' });
+        return;
+      }
+      res.send({ data: users });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(BAD_REQUEST_ERROR_STATUS).send({ message: 'Пользователь с указанным ID не найден' });
+      } else {
+        res.status(INTERNAL_SERVER_ERROR_STATUS).send({ message: 'Произошла ошибка' });
+      }
+    });
 };
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-  .then((users) => res.status(CREATE_STATUS).send({ data: users }))
-  .catch((err) => {
-    if (err.name === 'ValidationError') {
-      res.status(BAD_REQUEST_ERROR_STATUS).send({ message: 'Переданы некорректные данные' });
-    } else {
-      res.status(INTERNAL_SERVER_ERROR_STATUS).send({ message: 'Ошибка сервера' });
-    }
-  });
+    .then((users) => res.status(CREATE_STATUS).send({ data: users }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_ERROR_STATUS).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(INTERNAL_SERVER_ERROR_STATUS).send({ message: 'Ошибка сервера' });
+      }
+    });
 };
 
 const updateProfile = (req, res) => {
   const updateData = req.body;
   User.findByIdAndUpdate(req.user._id, updateData, { new: true, runValidators: true })
-  .then((users) => {
-    if (!users) {
-      res.status(NOT_FOUND_ERROR_STATUS).send({ message: 'Пользователь не найден' });
-      return;
-    }
-    res.send({ data: users });
-  })
-  .catch((err) => {
-    if (err.name === 'ValidationError') {
-      res.status(BAD_REQUEST_ERROR_STATUS).send({ message: 'Переданы некорректные данные' });
-    } else if (err.name === 'CastError') {
-      res.status(BAD_REQUEST_ERROR_STATUS).send({ message: 'Пользователь с указанным ID не найден' });
-    } else {
-      res.status(INTERNAL_SERVER_ERROR_STATUS).send({ message: 'Ошибка сервера' });
-    }
-  });
+    .then((users) => {
+      if (!users) {
+        res.status(NOT_FOUND_ERROR_STATUS).send({ message: 'Пользователь не найден' });
+        return;
+      }
+      res.send({ data: users });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_ERROR_STATUS).send({ message: 'Переданы некорректные данные' });
+      } else if (err.name === 'CastError') {
+        res.status(BAD_REQUEST_ERROR_STATUS).send({ message: 'Пользователь с указанным ID не найден' });
+      } else {
+        res.status(INTERNAL_SERVER_ERROR_STATUS).send({ message: 'Ошибка сервера' });
+      }
+    });
 };
 
 const updateProfileAvatar = (req, res) => {
   const updateData = req.body;
   User.findByIdAndUpdate(req.user._id, updateData, { new: true, runValidators: true })
-  .then((users) => {
-    if (!users) {
-      res.status(NOT_FOUND_ERROR_STATUS).send({ message: 'Пользователь не найден' });
-      return;
-    }
-    res.send({ data: users });
-  })
-  .catch((err) => {
-    if (err.name === 'ValidationError') {
-      res.status(BAD_REQUEST_ERROR_STATUS).send({ message: 'Переданы некорректные данные' });
-    } else if (err.name === 'CastError') {
-      res.status(BAD_REQUEST_ERROR_STATUS).send({ message: 'Пользователь с указанным ID не найден' });
-    } else {
-      res.status(INTERNAL_SERVER_ERROR_STATUS).send({ message: 'Ошибка сервера' });
-    }
-  });
+    .then((users) => {
+      if (!users) {
+        res.status(NOT_FOUND_ERROR_STATUS).send({ message: 'Пользователь не найден' });
+        return;
+      }
+      res.send({ data: users });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_ERROR_STATUS).send({ message: 'Переданы некорректные данные' });
+      } else if (err.name === 'CastError') {
+        res.status(BAD_REQUEST_ERROR_STATUS).send({ message: 'Пользователь с указанным ID не найден' });
+      } else {
+        res.status(INTERNAL_SERVER_ERROR_STATUS).send({ message: 'Ошибка сервера' });
+      }
+    });
 };
 
 module.exports = {
